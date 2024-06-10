a6087f399985d833d5cc2d67b8ddb57f4361b565
ToxxNll <dolayevtohtar@gmail.com>
2024-06-10 21:48:34 +0500

    Preparing
    Preparing workflow as additional repositories for users and pizzas

---
+++ pizza_time/packages/user_repository/lib/src/user_repo.dart
@@ -0,0 +1,13 @@
+abstract class UserRepositoty{
+
+  Stream <MyUser> get user;
+
+  Future <MyUser> signUp(MyUser myUser, String password);
+
+  Future<void> setUserData(MyUser user);
+
+  Future<void>signIn(String email, String password);
+  
+  Future<void> logOut();
+
+}
